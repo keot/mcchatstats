b7691ce0e1ee70cf340c70d1ee09b39aa5e8a524
James Mardell <james.mardell@imperial.ac.uk>
2012-12-03 18:51:21 +0000

    Added the GPL license header to the main application file.

--- mcchatstats.py
+++ mcchatstats.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+# MCchatstats - Converts a Minecraft (MSM) chat log-file into an HTML document
+# full of statistics
+# Copyright (c) 2012 James Mardell
+
+# MCchatstats is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# MCchatstats is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# MCchatstats. If not, see <http://www.gnu.org/licenses/>.
+
 import sys, math
 from datetime import datetime
 
